e2d3c28c290494f875ce3c47f6e93b0ddfdaba09
Benjamin <bejaminbamidele63@gmail.com>
2022-11-02 04:58:42 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new a line
+ * @s: string
+*/
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
